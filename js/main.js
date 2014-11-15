b17ec2ff91e1ca9341153f65479ee274dcd5e3d5
sylasjr <sylas@GEMCO-MAC-08.local>
2014-11-15 04:35:56 -0600

    Track scores and style changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 
 $( document ).ready(function() {
     // $("#gift").fadeOut();
+       var playerVal, 
+        compVal,
+        playerScore = 1,
+        compScore = 1;
 
     $('#gift').bind('change', function () {
 
@@ -15,9 +19,11 @@ $( document ).ready(function() {
     });
     bannerImage();
 
-    var playerVal, compVal = Math.floor((Math.random() * 3) + 1);
 
     $('.game .player .btn').on('click',function(){
+       $('.game .computer .btn').parent().children().removeClass("btn-warning").css('opacity','0.3');
+
+
         $(this).parent().children().removeClass("btn-success");
         $(this).addClass("btn-success");
         $('.computer img').css('opacity','0.3');
@@ -35,14 +41,16 @@ $( document ).ready(function() {
           playerVal=3;
         }
 
-        computerChoice();
-
-
-
+        randomNumber ();
     })
 
+    function randomNumber() {
+         compVal =  (Math.floor(Math.random()*3)+1);
+    }
+
     function computerChoice (){
-      $('.game .computer .btn').parent().children().removeClass("btn-success");
+      $('.game .computer .btn').css('opacity','1');
+      $('.game .computer .btn').parent().children().removeClass("btn-warning");
       if(compVal == 1){
         $('.game .computer .paper').addClass("btn-warning");
       }
@@ -50,8 +58,32 @@ $( document ).ready(function() {
         $('.game .computer .rock').addClass("btn-warning");
       }
       else{
-        $('.game .computer .scissor').addClass("btn-warning");
+        $('.game .computer .scissors').addClass("btn-warning");
       }
+    }
+
+    function keepScore() {
+     var  playerPoint = $('.player .points'),
+          compPoint   = $('.computer .points');
+        if(playerScore == 2){
+            playerPoint.find('.one').addClass('playerPoint');
+        }
+        else if(playerScore == 3){
+            playerPoint.find('.two').addClass('playerPoint');
+        }
+        else if(playerScore == 4){
+            playerPoint.find('.three').addClass('playerPoint');
+        }
+
+        if(compScore == 2){
+            compPoint.find('.one').addClass('compPoint');
+        }
+        else if(compScore == 3){
+            compPoint.find('.two').addClass('compPoint');
+        }
+        else if(compScore == 4){
+            compPoint.find('.three').addClass('compPoint');
+        }
     }
 
 
@@ -59,36 +91,35 @@ $( document ).ready(function() {
     $('.game a').on('click', function(){
       // var playerVal  = $(".player .form-control").val(),
       // compVal        = $(".computer .form-control").val(),
-      paper          = 1,
+      computerChoice();
+
+      var paper      = 1,
       rock           = 2,
       scissors       = 3;
 
-      if(playerVal == paper && compVal == rock) {
-        $(".message").text("Player Wins");
+
+      if(playerVal == paper && compVal == rock || playerVal == rock && compVal == scissors || playerVal == scissors && compVal == paper ) {
+        $(".player .me").addClass("bounceIn animated").find('img').addClass('winner player').removeClass('challenge');;
+        setTimeout(function(){$(".me").removeClass("bounceIn animated").find('img').removeClass('winner player').addClass('challenge')}, 1000);
+        playerScore ++;
+        keepScore();
       }
-      else if (playerVal == rock && compVal == scissors) {
-        $(".message").text("Player Wins");
+      else if(playerVal == compVal) {
+        $(".game .me").addClass("wobble animated");
+        setTimeout(function(){$(".me").removeClass("wobble animated");}, 1000);
+
+
       }
-      else if (playerVal == scissors && compVal == paper) {
-        $(".message").text("Player Wins");
+      else if(compVal == paper && playerVal == rock || compVal == rock && playerVal == scissors || compVal == scissors && playerVal == paper) {
+        $(".computer .me").addClass("bounceIn animated").find('img').addClass('winner oponent').removeClass('challenge');
+        setTimeout(function(){$(".me").removeClass("bounceIn animated").find('img').removeClass('winner oponent').addClass('challenge')}, 1000);
+        compScore  ++;
+        keepScore();
+
       }
 
-      if(playerVal == compVal) {
-         $(".message").text("It is a tie");
-      }
-
-      if(compVal == paper && playerVal == rock) {
-        $(".message").text("Trent Wins");
-      }
-      else if (compVal == rock && playerVal == scissors) {
-        $(".message").text("Trent Wins");
-      }
-      else if (compVal == scissors && playerVal == paper) {
-       $(".message").text("Trent Wins");
-      }
-
-        $('.computer img').css('opacity','1');
-        $('.computer .me span').addClass('hidden');
+      $('.computer img').css('opacity','1');
+      $('.computer .me span').addClass('hidden');
 
 
 
